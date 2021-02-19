45f3eceebb3f5d7ecdbff72b962b8138da43304c
emilykmcullen <emilycullen135@gmail.com>
2021-02-19 16:50:48 +0000

    game and constants header created

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include "./Game.h"
+#include "./Constants.h"
 using namespace std;
 
 int main(int argc, char *args[]) {
+    Game *game = new Game();
+    
+    game->Initialize(WINDOW_WIDTH, WINDOW_HEIGHT);
 
-    cout << "Game is running..." << endl;
+    while (game -> IsRunning()){
+        game->ProcessInput();
+        game->Update();
+        game->Render();
+
+    } 
+
+    game->Destroy();
 
     return 0;
 }
