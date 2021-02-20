cde1a8b4384cd1e47c5c1ec4faabf657fee02968
emilykmcullen <emilycullen135@gmail.com>
2021-02-20 14:09:35 +0000

    changed game point in main

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,18 +4,17 @@
 using namespace std;
 
 int main(int argc, char *args[]) {
-    Game *game = new Game();
+    Game game;
     
-    game->Initialize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    game.Initialize(WINDOW_WIDTH, WINDOW_HEIGHT);
 
-    while (game -> IsRunning()){
-        game->ProcessInput();
-        game->Update();
-        game->Render();
-
+    while (game.IsRunning()){
+        game.ProcessInput();
+        game.Update();
+        game.Render();
     } 
 
-    game->Destroy();
+    game.Destroy();
 
     return 0;
 }
