55c5fb4ddd4eae81a5efa6a514aa7991ab41b470
emilykmcullen <emilycullen135@gmail.com>
2021-02-23 12:47:15 +0000

    entitymanager.cpp

--- src/EntityManager.h
+++ src/EntityManager.h
@@ -9,6 +9,7 @@ class EntityManager {
     private:
         std::vector<Entity*> entities;
     public:
+        void ClearData();
         void Update(float deltaTime);
         void Render();
         bool HasNoEntities();
