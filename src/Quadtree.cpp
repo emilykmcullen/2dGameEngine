f77240b6631515164a445de6a795cea9468f680e
emilykmcullen <emilycullen135@gmail.com>
2021-04-17 08:57:54 +0100

    node splitting method

--- src/Quadtree.cpp
+++ src/Quadtree.cpp
@@ -17,3 +17,22 @@ void Quadtree::clear(){
         }
     }
 }
+
+//SPLIT THE NODE INTO 4 SUBNODES
+//Divides the nodes into 4 equal parts and initializes the subnodes with the new bounds
+void Quadtree::split(){
+    int subWidth = static_cast<int>(bounds.w/2);
+    int subHeight = static_cast<int>(bounds.h/2);
+    int x = static_cast<int>(bounds.x);
+    int y = static_cast<int>(bounds.y);
+
+    SDL_Rect rectZero{x + subWidth, y, subWidth, subHeight};
+    SDL_Rect rectOne{x + subWidth, y, subWidth, subHeight};
+    SDL_Rect rectTwo{x + subWidth, y, subWidth, subHeight};
+    SDL_Rect rectThree{x + subWidth, y, subWidth, subHeight};
+    
+    nodes[0] = new Quadtree(level + 1,  rectZero);
+    nodes[1] = new Quadtree(level + 1,  rectOne);
+    nodes[2] = new Quadtree(level + 1,  rectTwo);
+    nodes[3] = new Quadtree(level + 1,  rectThree);
+}
--- src/Quadtree.h
+++ src/Quadtree.h
@@ -3,6 +3,8 @@
 #include <SDL2/SDL.h>
 #include<vector>
 #include <list>
+#include <memory>
+using namespace std;
 
 class Quadtree {
     private:
@@ -10,12 +12,12 @@ class Quadtree {
         int maxLevels = 5;
 
         int level; //current node level
-        std::vector<SDL_Rect*> objects;
+        std::vector<unique_ptr<SDL_Rect>> objects;
         SDL_Rect bounds;
         Quadtree* nodes[4];
         //using an array because I know how many elements I need
         //also remember vectors/arrays cannot hold references!
-        void split();
+        
         int getIndex(SDL_Rect& pRect);
     
     public:
@@ -23,6 +25,7 @@ class Quadtree {
         void clear();
         void insert(SDL_Rect& pRect);
         std::vector<SDL_Rect*> retrieve;
+        void split();
         
 
         
