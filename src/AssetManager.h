930a66c1dd9d60c7c526ad2dbd3d6db66a774abc
emilykmcullen <emilycullen135@gmail.com>
2021-04-20 12:48:36 +0100

    added AddFont and GetFont to AssetManager and created FontManager

--- src/AssetManager.h
+++ src/AssetManager.h
@@ -3,19 +3,29 @@
 
 #include <map>
 #include <string>
+#include <SDL2/SDL_ttf.h>
 #include "./TextureManager.h"
+#include "./FontManager.h"
 #include "./EntityManager.h"
+
+
 
 class AssetManager {
     private:
         EntityManager* manager;
         std::map<std::string, SDL_Texture*> textures;
+        std::map<std::string, TTF_Font*> fonts;
     public:
         AssetManager(EntityManager* manager);
         ~AssetManager();
         void ClearData();
+
+        //passing const char* as this is what SDL expects (otherwise would be std::string )
         void AddTexture(std::string textureId, const char* filePath);
         SDL_Texture* GetTexture(std::string textureId);
+
+        void AddFont(std::string fontId,  const char* filePath, int fontSize);
+        TTF_Font* GetFont(std::string fontId);
 };
 
 #endif
